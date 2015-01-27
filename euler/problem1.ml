e855e9f30ef8c99bf0c6e89051f6d9d0b3593e3e
io <brian.downs@gmail.com>
2015-01-27 14:17:09 -0700

    problem 1 solved

--- euler/problem1.ml
+++ euler/problem1.ml
@@ -4,5 +4,11 @@
   Find the sum of all the multiples of 3 or 5 below 1000.
  *)
 
-let f =
-    print_int (List.fold_left (+) (List.filter (fun x -> (x mod 3) = 0 || (x mod 5) = 0) (range 1 999)));
+let rec build_list s e =
+    let x = s+1 in
+    if s <= e then s::(build_list x e) else [];;
+
+let problem el =
+    List.fold_left (+) 0 (List.filter (fun x -> (x mod 3) = 0 || (x mod 5) = 0) el);;
+
+print_int (problem (build_list 0 999));;
